eed27eeb7c831ff45fe5eec09d59080d874fa958
Sch3lp <sch3lp@gmail.com>
2024-04-14 10:57:57 +0200

    Tidying

--- src/main/kotlin/be/kunlabora/crafters/kunlaquota/rest/WebConfig.kt
+++ src/main/kotlin/be/kunlabora/crafters/kunlaquota/rest/WebConfig.kt
@@ -2,7 +2,6 @@ package be.kunlabora.crafters.kunlaquota.rest
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.function.router
@@ -18,10 +17,4 @@ class WebConfig : WebMvcConfigurer {
     fun apiRoutesBean(quotes: Quotes) = router {
         "/api".nest(apiRoutes(quotes))
     }
-
-    override fun configureMessageConverters(converters: List<HttpMessageConverter<*>>) {
-        // configure message conversion...
-    }
-
-
 }
