b9acb39005dda5b1555c2f7307f5e6d69490b844
Sch3lp <sch3lp@gmail.com>
2024-06-21 16:38:19 +0200

    Extracted some stuff to "components" and added Bulma

--- src/main/kotlin/be/kunlabora/crafters/kunlaquota/web/ui/ui.kt
+++ src/main/kotlin/be/kunlabora/crafters/kunlaquota/web/ui/ui.kt
@@ -1,9 +1,8 @@
 package be.kunlabora.crafters.kunlaquota.web.ui
 
 import be.kunlabora.crafters.kunlaquota.service.IQuotes
-import kotlinx.html.body
-import kotlinx.html.html
-import kotlinx.html.p
+import be.kunlabora.crafters.kunlaquota.service.domain.Quote
+import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -14,29 +13,51 @@ import java.time.format.DateTimeFormatter
 
 fun uiRoutes(quotes: IQuotes): RouterFunctionDsl.() -> Unit = {
     GET("") {
+        val title = "KunlaQuota"
         ServerResponse.status(HttpStatus.OK)
             .contentType(MediaType.TEXT_HTML)
             .body(
-                StringWriter().appendHTML().html {
-                    body {
-                        val fetchResult = quotes.findAll()
-                        fetchResult
-                            .map { quotes ->
-                                quotes.forEach { quote ->
-                                    p {
-                                        p { +"-------${quote.at.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)}-----------" }
-                                        p {
-                                            quote.lines.forEach { line ->
-                                                +"${line.name}: ${line.text}"
-                                            }
-                                        }
-                                        p { +"----------------------------------------" }
-                                    }
-                                }
-                            }
-                    }
+                wrapper(title) {
+                    showQuotes(quotes)
                 }
-                    .toString()
             )
+    }
+}
+
+fun wrapper(title: String, block: DIV.() -> Unit) =
+    StringWriter().appendHTML().html {
+        head {
+            title { +title }
+            meta(charset = "utf-8")
+            meta(name = "viewport", content = "width=device-width, initial-scale=1")
+            link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css")
+        }
+        body {
+            section(classes = "section") {
+                div(classes="container") {
+                    block()
+                }
+            }
+        }
+    }.toString()
+
+private fun FlowContent.showQuotes(quotes: IQuotes) {
+    quotes.findAll()
+        .map { fetchedQuotes ->
+            fetchedQuotes.forEach { quote ->
+                showQuote(quote)
+            }
+        }
+}
+
+private fun FlowContent.showQuote(quote: Quote) {
+    p {
+        p { +"-------${quote.at.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)}-----------" }
+        p {
+            quote.lines.forEach { line ->
+                +"${line.name}: ${line.text}"
+            }
+        }
+        p { +"----------------------------------------" }
     }
 }
