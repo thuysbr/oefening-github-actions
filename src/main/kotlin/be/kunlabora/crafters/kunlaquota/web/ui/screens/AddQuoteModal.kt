a2fcd386ad4fd7e37e64a41333f0fb575be65126
Sch3lp <sch3lp@gmail.com>
2024-07-06 22:52:25 +0200

    Some refactoring

--- src/main/kotlin/be/kunlabora/crafters/kunlaquota/web/ui/screens/AddQuoteModal.kt
+++ src/main/kotlin/be/kunlabora/crafters/kunlaquota/web/ui/screens/AddQuoteModal.kt
@@ -17,55 +17,83 @@ object AddQuoteModal {
             div(classes = "modal-background")
             div(classes = "modal-card") {
                 style = "border-radius: 8px;" // Ensure consistent rounded corners
-                header(classes = "modal-card-head") {
-                    style = "border-bottom: none;" // Remove the gradient
-                    p(classes = "modal-card-title") { +"Add a Quote" }
-                    button(classes = "delete") {
-                        id = "delete"
-                        attributes["aria-label"] = "close"
-                        hyper = "on click take .is-active from $addQuoteModalId wait 200ms then remove $addQuoteModalId"
-                    }
+                header()
+                form()
+            }
+        }
+    }
+
+    private fun DIV.form() {
+        form {
+            hxTarget = "#errorMessages"
+            hxSwap = "innerHTML"
+            hxPost = baseUiUrl.path("new")
+            hyper = "on submit take .is-active from $addQuoteModalId"
+
+            section(classes = "modal-card-body") {
+                div("field") {
+                    id = "extraLine"
+                    addQuoteLine()
                 }
-                form {
-                    hxTarget = "#errorMessages"
-                    hxSwap = "innerHTML"
-                    hxPost = baseUiUrl.path("new")
-                    hyper = "on submit take .is-active from $addQuoteModalId"
 
-                    section(classes = "modal-card-body") {
-                        div("field") {
-                            id = "extraLine"
-                            addQuoteLine()
-                        }
-
-                        div("control has-text-centered") {
-                            button(classes = "button is-link is-light is-large") {
-                                type = ButtonType.button
-                                hxPost = baseUiUrl.path("new", "addLine")
-                                hxTarget = "#extraLine"
-                                hxSwap = "beforeend"
-                                span(classes = "icon") {
-                                    i(classes = "fas fa-plus")
-                                }
-                            }
-                        }
-                    }
-                    footer(classes = "modal-card-foot is-justify-content-end") {
-                        style = "border-top: none; display: flex; justify-content: flex-end; width: 100%;"
-                        button(classes = "button is-primary is-success") {
-                            type = ButtonType.submit
-                            style = "margin-right: 0.5rem;"
-                            +"Save"
-                        }
-                        button(classes = "button") {
-                            type = ButtonType.button
-                            hyper =
-                                "on click take .is-active from $addQuoteModalId wait 200ms then remove $addQuoteModalId"
-                            +"Cancel"
-                        }
-                    }
+                div("control has-text-centered") {
+                    addExtraLineButton()
                 }
             }
+            footer()
+        }
+    }
+
+    private fun DIV.addExtraLineButton() {
+        button(classes = "button is-link is-light is-large") {
+            type = ButtonType.button
+            hxPost = baseUiUrl.path("new", "addLine")
+            hxTarget = "#extraLine"
+            hxSwap = "beforeend"
+            span(classes = "icon") {
+                i(classes = "fas fa-plus")
+            }
+        }
+    }
+
+    private fun FORM.footer() {
+        footer(classes = "modal-card-foot is-justify-content-end") {
+            style = "border-top: none; display: flex; justify-content: flex-end; width: 100%;"
+            submitButton()
+            cancelButton()
+        }
+    }
+
+    private fun FOOTER.submitButton() {
+        button(classes = "button is-primary is-success") {
+            type = ButtonType.submit
+            style = "margin-right: 0.5rem;"
+            +"Save"
+        }
+    }
+
+    private val removeModalHypertext = "on click take .is-active from $addQuoteModalId wait 200ms then remove $addQuoteModalId"
+    private fun FOOTER.cancelButton() {
+        button(classes = "button") {
+            type = ButtonType.button
+            hyper = removeModalHypertext
+            +"Cancel"
+        }
+    }
+
+    private fun HEADER.closeButton() {
+        button(classes = "delete") {
+            id = "delete"
+            attributes["aria-label"] = "close"
+            hyper = removeModalHypertext
+        }
+    }
+
+    private fun DIV.header() {
+        header(classes = "modal-card-head") {
+            style = "border-bottom: none;" // Remove the gradient
+            p(classes = "modal-card-title") { +"Add a Quote" }
+            closeButton()
         }
     }
 
